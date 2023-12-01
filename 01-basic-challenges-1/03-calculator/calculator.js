4cf463a02e36057c4342889dd776aa6963ed24d9
Echchaoui <m.echchaoui007@gmail.com>
2023-12-01 22:10:46 +0100

    calculator basic one

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,24 @@
-function calculator() {}
+function calculator(a,b,op) {
+    let result=0;
+    switch (op) {
+        case '+':
+            result= a+b;
+            break;
+        case '-':
+            result=a-b;
+            break;
+        case '*':
+            result= a*b;
+            break;
+        case '/':
+            result=a/b;
+            break;
+        
+
+    
+        default:
+            break;
+    }
+}
 
 module.exports = calculator;
