aa45a8d815d559ba526cceaa08f02048df46ecb4
Echchaoui <m.echchaoui007@gmail.com>
2023-12-03 17:33:48 +0100

    correcting_display likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -5,10 +5,12 @@ function displayLikes(names) {
         return 'no one likes this';
     }else if(length === 1){
         return `${names[0]} likes this`
-    }else if(length===2){
-        return `${names[0]} and ${names[1]} likes this`
+    } else if(length===2){
+        return `${names[0]} and ${names[1]} like this`
+    }else if(length===3){
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
     }else{
-        return `${names[0]} and ${names[1]} and ${length - 2} others likes this`
+        return `${names[0]}, ${names[1]} and ${length - 2} others like this`
     }
 
 
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,13 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (arr.length === 0) return 1;
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+
+  let actuelSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    actuelSum += arr[i];
+  }
+  return expectedSum - actuelSum;
+}
 
 module.exports = findMissingNumber;
