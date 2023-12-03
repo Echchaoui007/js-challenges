fb1b33072da708a741cc0f750a1600110ed9bc76
Echchaoui <m.echchaoui007@gmail.com>
2023-12-03 16:58:25 +0100

    display_likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,17 @@
-function displayLikes() {}
+function displayLikes(names) {
+    const length =names.length
+
+    if(length === 0){
+        return 'no one likes this';
+    }else if(length === 1){
+        return `${names[0]} likes this`
+    }else if(length===2){
+        return `${names[0]} and ${names[1]} likes this`
+    }else{
+        return `${names[0]} and ${names[1]} and ${length - 2} others likes this`
+    }
+
+
+}
 
 module.exports = displayLikes;
