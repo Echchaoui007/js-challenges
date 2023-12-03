e99248af2489b36110926914429f7d83974be066
Echchaoui <m.echchaoui007@gmail.com>
2023-12-03 19:40:28 +0100

    find_missing_letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,14 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const stratIndex = alphabet.indexOf(arr[0]);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== alphabet[stratIndex + i]) {
+      return alphabet[stratIndex + i];
+    }
+  }
+
+  return "";
+}
 
 module.exports = findMissingLetter;
