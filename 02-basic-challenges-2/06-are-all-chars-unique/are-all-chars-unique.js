18ec1d9813d85a84c959ba986194761809462ec3
Echchaoui Mohammed <m.echchaoui007@gmail.com>
2023-12-13 18:40:01 +0100

    unique_caracters

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,16 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+    const charCount={};
+
+    for (let i=0;i<str.length;i++){
+        const char = str[i];
+        if(charCount[char]){
+            return false;
+        }
+
+        charCount[char] = true;
+
+    }
+    return true;
+}
 
 module.exports = areAllCharactersUnique;
