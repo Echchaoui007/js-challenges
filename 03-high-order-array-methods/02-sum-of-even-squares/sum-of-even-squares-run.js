e5a7758a6b3b7edf5e076f94443edcda05476f66
Echchaoui Mohammed <m.echchaoui007@gmail.com>
2023-12-14 15:20:20 +0100

    sum_of-even_squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
@@ -1,5 +1,5 @@
 const sumOfEvenSquares = require('./sum-of-even-squares');
 
-const result = sumOfEvenSquares([1, 2, 3, 4, 5, 6]);
+const result = sumOfEvenSquares([-2,-1,1, 2, 3, 4, 5, 6,7,8,9]);
 
 console.log(result);
--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,9 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evenNumbers = numbers.filter((num) => num % 2 === 0);
+  const squaresNumbers = evenNumbers.map((num) => num ** 2);
+  const sumOfNumbers = squaresNumbers.reduce((total, num) => total + num, 0);
+
+  return sumOfNumbers;
+}
 
 module.exports = sumOfEvenSquares;
