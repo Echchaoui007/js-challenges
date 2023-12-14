1cb844ade4b4e0a63b7c4e55452704d8a30f28fd
Echchaoui Mohammed <m.echchaoui007@gmail.com>
2023-12-14 15:42:53 +0100

    calculate_totale_salesWithTaxes

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,15 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+  const totalSales = products.reduce(
+    (sum, product) => sum + product.price * product.quantity,
+    0
+  );
+
+  const taxAmount=(totalSales * tax)/100;
+
+  const totalSalesWithTaxes= totalSales+taxAmount;
+
+
+  return parseFloat(totalSalesWithTaxes.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
