632f3b5555757806efccf5074f9ef9b05db6855d
Echchaoui Mohammed <m.echchaoui007@gmail.com>
2023-12-14 19:40:37 +0100

    highest_score_ofWords

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,22 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const worlds = str.split(" ");
+  const scores = worlds.map((word) => {
+    let score = 0;
+    for (let letters of word) {
+      score += letters.charCodeAt(0) - 96;
+    }
+    return score;
+  });
+  let highestScore = 0;
+  let highestIndexe = 0;
+
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] > highestScore) {
+      highestScore = scores[i];
+      highestIndexe = i;
+    }
+  }
+  return worlds[highestIndexe];
+}
 
 module.exports = highestScoringWord;
