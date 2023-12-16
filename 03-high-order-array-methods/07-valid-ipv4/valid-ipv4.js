a93797c7da4ec0250600e4d045e56851e7be2171
Echchaoui Mohammed <m.echchaoui007@gmail.com>
2023-12-16 19:17:18 +0100

    valid_ipv4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,15 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (input) => {
+    const octest= input.split('.')
+    if(octest.length !== 4){
+        return false
+    }
+    return octest.every((octet)=>{
+        const num =parseInt(octet)
+        return num >=0 && num <= 255 && octet == num.toString();
+    })
+
+
+    
+};
 
 module.exports = isValidIPv4;
