2c4ec8d6b5b0d002c82f50653702c4d77d1ea30b
Echchaoui Mohammed <m.echchaoui007@gmail.com>
2023-12-20 01:54:21 +0100

    number rang recursion

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,12 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  if (endNum === startNum) {
+    return [startNum];
+  }
+
+  const numbers = numberRange(startNum, endNum - 1);
+  numbers.push(endNum);
+  console.log(numbers);
+  return numbers;
+}
 
 module.exports = numberRange;
